fd7043fe7f510cd45031517b7d878c00bf302532
Carlos Ramirez <carlosdanielrt@gmail.com>
2020-04-06 01:48:20 -0400

    [ADD] index.js: basic file structure

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// initializations
+
+const app = express();
+
+// settings
+
+// middlewares
+
+// required for passport
+
+// routes
+
+// static files
+
+// start the server
+
+
