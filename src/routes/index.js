d7d675c524da4176c190333b4706bf082c731ba0
Carlos Ramirez <carlosdanielrt@gmail.com>
2020-04-06 02:55:51 -0400

    [ADD] routes: login route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,18 @@
 const { Router } = require('express');
 const router = Router();
 
+// Index page route
+
 router.get('/', (req, res) => {
     res.send('Index Page');
+});
+
+// Login page route
+
+router.get('/login', (req, res) => {
+    res.render('login.ejs', {
+        message: req.flash('loginMessage')
+    });
 });
 
 module.exports = router;
