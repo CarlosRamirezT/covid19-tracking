683567e2421064d18b928e8bd010db6367a4837a
Carlos Ramirez <carlosdanielrt@gmail.com>
2020-04-06 19:53:15 -0400

    [ADD] routes: rest of basic routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,21 +2,88 @@
 const { Router } = require('express');
 const router = Router();
 
-// Index page route
+// Index page routes
+// initial route for no signed users
 
-router.get('/', (req, res) => {
-    res.send('Index Page');
+router.get('/', async(req, res) => {
+    res.send('Index Page for no signed users');
 });
 
-// Login page route
+// Index page routes
+// initial route for signed users
+
+router.get('/user/:id', async(req, res) => {
+    res.send('Index Page for signed users');
+});
+
+// Login page routes
 
 router.get('/login', (req, res) => {
     res.send('Login Page')
 });
 
-// signup page route
+// signup page routes
+
 router.get('/signup', (req, res) => {
     res.send('Sign Up Page')
+});
+
+// intermediate page routes
+
+router.get('/intermediate', (req, res) => {
+    res.send('Intermediate Page')
+});
+
+// intermediate upload page routes
+
+router.get('/intermediate/upload', (req, res) => {
+    res.send('Intermediate Upload Page')
+});
+
+// profile page routes
+
+router.get('/profile', (req, res) => {
+    res.send('Profile Page')
+});
+
+// logout routes
+
+router.get('/logout', (req, res) => {
+    res.send('Logout Page')
+});
+
+// signed users cases upload
+
+router.get('/user/upload/:id', async(req, res) => {
+    res.send('Upload Page');
+});
+
+router.post('/user/upload/:id', async(req, res) => {
+    res.send('Upload Page');
+});
+
+// map tracking routes
+
+router.get('/map', async(req, res) => {
+    res.send('Map Page');
+});
+
+// case profile route
+
+router.get('/case/:id', async(req, res) => {
+    res.send('Case Profile Page');
+});
+
+// case delete cases route
+
+router.get('/case/:id/delete', async(req, res) => {
+    res.send('Case visualize and delete Page');
+});
+
+// case visualize and delete cases route
+
+router.get('/user/delete/case', async(req, res) => {
+    res.send('Case visualize and delete Page');
 });
 
 module.exports = router;
